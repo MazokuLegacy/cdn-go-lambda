cc409b251fa0d263b83fd4fe3c43c0f66635d03a
Nark <sooryarox2004@gmail.com>
2024-10-12 00:31:47 +0300

    try this one more time

--- lambda/main.go
+++ lambda/main.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"log"
 )
 
-func LambdHandler(ctx context.Context, event events.LambdaFunctionURLRequest) {
-	log.Println(event)
-	log.Println("hey ther")
-	return
+func LambdHandler(ctx context.Context, event events.LambdaFunctionURLRequest) (string, error) {
+	eventJSON, err := json.Marshal(event)
+	if err != nil {
+		return "", fmt.Errorf("failed to marshal event: %v", err)
+	}
+	log.Printf("Received event: %s", eventJSON)
+	return "Event processed successfully", nil
 }
 
 func main() {
