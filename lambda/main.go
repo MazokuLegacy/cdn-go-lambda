e27231e58831a50e1d16e113d15bcdb68f8c979f
Nark <sooryarox2004@gmail.com>
2024-10-14 01:06:35 +0300

    debugg

--- lambda/main.go
+++ lambda/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math"
 	"os"
 	"os/exec"
 	"strconv"
@@ -135,7 +136,7 @@ func pngToWebp(input []byte, width int) ([]byte, error) {
 }
 
 func getScale(width int) string {
-	return "scale=iw/" + strconv.Itoa(1500/width) + ":" + "ih/" + strconv.Itoa(2100*(width/1500))
+	return "scale=iw/" + strconv.Itoa(int(math.Ceil(float64(1500)/float64(width)))) + ":" + "ih/" + strconv.Itoa(2100*int(math.Ceil(float64(width)/float64(1500))))
 }
 
 func webmToGif(input []byte, width int) ([]byte, error) {
