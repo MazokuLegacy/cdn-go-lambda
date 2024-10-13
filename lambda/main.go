d327041b811135f7a371371e3df0e543b9eb3d5d
Nark <sooryarox2004@gmail.com>
2024-10-13 20:47:36 +0300

    switching over to commandline

--- lambda/main.go
+++ lambda/main.go
@@ -117,20 +117,37 @@ func getWebpFromWebm(input []byte) ([]byte, error) {
 }
 
 func convertWebMToMP4(input []byte) ([]byte, error) {
-	inputReader := bytes.NewReader(input)
-	filePath := "/tmp/output.mp4"
-	file, err := os.Create(filePath)
+	inPath := "/tmp/input.webm"
+	outPath := "/tmp/output.mp4"
+	inFile, err := os.Create(inPath)
 	if err != nil {
 		return nil, err
 	}
-	defer file.Close()
-	defer os.Remove(filePath)
-	err = fluentffmpeg.NewCommand("").VideoCodec("libvpx-vp9").PipeInput(inputReader).OutputFormat("mp4").OutputPath(filePath).Overwrite(true).Run()
-
+	defer inFile.Close()
+	defer os.Remove(inPath)
+	outFile, err := os.Create(outPath)
 	if err != nil {
 		return nil, err
 	}
-	output, err := io.ReadAll(file)
+	defer outFile.Close()
+	defer os.Remove(outPath)
+	inFile.Write(input)
+	cmd := exec.Command("ffmpeg", "-codec:v", "libvpx-vp9", "-y", "-i", inPath, outPath)
+	if err != nil {
+		return nil, err
+	}
+	err = cmd.Start()
+	if err != nil {
+		fmt.Println("Error starting command:", err)
+		return nil, err
+	}
+	err = cmd.Wait()
+	if err != nil {
+		fmt.Println("Error waiting for command:", err)
+		return nil, err
+	}
+	log.Println("completed")
+	output, err := io.ReadAll(outFile)
 	return output, nil
 }
 
