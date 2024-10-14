a4f4ed6705e97b60ba7e6f3c76466a3594b98897
Nark <sooryarox2004@gmail.com>
2024-10-14 09:33:47 +0300

    pls

--- lambda/framedWebm.go
+++ lambda/framedWebm.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"log"
@@ -36,17 +37,39 @@ func framedWebm(input []byte, frame []byte) ([]byte, error) {
 	cmd := exec.Command("ffmpeg",
 		"-c:v", "libvpx-vp9",
 		"-i", inPath,
-		"-c:v", "libvpx-vp9",
 		"-i", framePath,
 		"-c:a", "copy",
 		"-filter_complex", "[0:v][1:v] overlay=0:0:enable='between(t,0,20)'",
 		"-y",
 		outPath)
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, err
+	}
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return nil, err
+	}
 	err = cmd.Start()
 	if err != nil {
 		fmt.Println("Error starting command:", err)
 		return nil, err
 	}
+	// Function to read and log output in real-time
+	go func(reader io.ReadCloser) {
+		scanner := bufio.NewScanner(reader)
+		for scanner.Scan() {
+			log.Println(scanner.Text()) // Logs each line of output as it happens
+		}
+	}(stdout)
+
+	// Function to read and log errors in real-time
+	go func(reader io.ReadCloser) {
+		scanner := bufio.NewScanner(reader)
+		for scanner.Scan() {
+			log.Println(scanner.Text()) // Logs each line of errors as they happen
+		}
+	}(stderr)
 	err = cmd.Wait()
 	if err != nil {
 		fmt.Println("Error waiting for command:", err)
