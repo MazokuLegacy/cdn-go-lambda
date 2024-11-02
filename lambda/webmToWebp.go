f865f4624e1819a06ac435f04fd70197204bfcfa
Nark <sooryarox2004@gmail.com>
2024-11-02 20:28:20 +0200

    reverting

--- lambda/webmToWebp.go
+++ lambda/webmToWebp.go
@@ -28,7 +28,6 @@ func webmToWebp(input []byte, width int) ([]byte, error) {
 	cmd := exec.Command("ffmpeg",
 		"-codec:v", "libvpx-vp9",
 		"-y", "-i", inPath,
-		"-codec:v", "libwebp",
 		"-vf", scale+":flags=lanczos",
 		"-loop", "0",
 		outPath)
